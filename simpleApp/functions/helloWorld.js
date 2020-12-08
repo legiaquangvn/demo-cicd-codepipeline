4faced76a8ea403a2f410975093e9065e6483b35
Quang Gia Le <legiaquangvn@yahoo.com>
2020-12-08 00:52:10 -0500

    update HelloWorld to v3

--- simpleApp/functions/helloWorld.js
+++ simpleApp/functions/helloWorld.js
@@ -1,3 +1,3 @@
 exports.handler =  (event, context, callback) => {
-    return callback(null, { statusCode: 200, body: "Hello World v2"});
+    return callback(null, { statusCode: 200, body: "Hello World v3"});
 };
